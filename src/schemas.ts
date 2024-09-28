896e456f5b7f1e67b7638e89396d69ab5c52605d
sahand-sn <109898282+sahandsn@users.noreply.github.com>
2024-09-28 20:22:48 +0330

    fix: translated schema validation error

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { TranslateFn } from "./types";
 
 const USERNAME_MAX = 50;
 const USERNAME_MIN = 2;
@@ -7,20 +8,28 @@ const PASSWORD_MAX = 50;
 const PASSWORD_MIN = 2;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const loginFormSchemaGenerator = (t: any) =>
+export const loginFormSchemaGenerator = (t: TranslateFn<"Schemas.LoginForm">) =>
   z.object({
     username: z
       .string({
         required_error: t("username-required"),
         invalid_type_error: t("username-type"),
       })
-      .min(USERNAME_MIN, { message: t("username-min", { min: USERNAME_MIN }) })
-      .max(USERNAME_MAX, { message: t("username-max", { max: USERNAME_MAX }) }),
+      .min(USERNAME_MIN, {
+        message: t("username-min", { min: USERNAME_MIN }),
+      })
+      .max(USERNAME_MAX, {
+        message: t("username-max", { max: USERNAME_MAX }),
+      }),
     password: z
       .string({
         required_error: t("password-required"),
         invalid_type_error: t("password-type"),
       })
-      .min(PASSWORD_MIN, { message: t("password-min", { min: PASSWORD_MIN }) })
-      .max(PASSWORD_MAX, { message: t("password-max", { max: PASSWORD_MAX }) }),
+      .min(PASSWORD_MIN, {
+        message: t("password-min", { min: PASSWORD_MIN }),
+      })
+      .max(PASSWORD_MAX, {
+        message: t("password-max", { max: PASSWORD_MAX }),
+      }),
   });
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,11 @@
 import { routing } from "@/i18n/routing";
+import { ReactElement, ReactNodeArray } from "react";
+import {
+  Formats,
+  TranslationValues,
+  MarkupTranslationValues,
+  RichTranslationValues,
+} from "next-intl";
 
 export interface IPageParams {
   params: { locale: string };
@@ -10,3 +17,40 @@ export interface IPageChildren {
 export type TLayout = IPageParams & IPageChildren;
 
 export type TInternalLink = keyof typeof routing.pathnames;
+
+// Utility type to extract keys under a given namespace
+type ExtractKeysUnderNamespace<
+  T,
+  N extends string,
+> = N extends `${infer K}.${infer Rest}`
+  ? K extends keyof T
+    ? ExtractKeysUnderNamespace<T[K], Rest>
+    : never
+  : N extends keyof T
+    ? T[N] extends object
+      ? keyof T[N] & string
+      : never
+    : never;
+
+export type TranslateFn<N extends string> = {
+  (
+    key: MessageKey<N>,
+    values?: TranslationValues,
+    formats?: Partial<Formats>,
+  ): string;
+  rich(
+    key: MessageKey<N>,
+    values?: RichTranslationValues,
+    formats?: Partial<Formats>,
+  ): string | ReactElement | ReactNodeArray;
+  markup(
+    key: MessageKey<N>,
+    values?: MarkupTranslationValues,
+    formats?: Partial<Formats>,
+  ): string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  raw(key: MessageKey<N>): any;
+};
+
+// Helper type to get valid keys under the namespace N
+type MessageKey<N extends string> = ExtractKeysUnderNamespace<IntlMessages, N>;
