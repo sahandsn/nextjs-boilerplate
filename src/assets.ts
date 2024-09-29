c6b72889b58c06719e8a1aab107900853c5ba6c7
sahandsn <sanaeisahand@gmail.com>
2024-09-30 00:17:08 +0330

    feat: translated images

--- src/assets.ts
+++ src/assets.ts
@@ -1,3 +1,5 @@
+import { TranslationFn } from "./types/translation-fn";
+
 const TITLE = "Create Next App";
 const TITLE_SHORT = "CNA";
 const TITLE_TEMPLATE = `%s - ${TITLE_SHORT}`;
@@ -7,12 +9,14 @@ import imgLogo from "#/logo.png";
 import imgHomePageScreenshot from "#/home-page-screenshot.png";
 import imgMyPageScreenshot from "#/my-page-screenshot.png";
 
-export {
-  TITLE,
-  TITLE_SHORT,
-  TITLE_TEMPLATE,
-  icoLogo,
-  imgLogo,
-  imgHomePageScreenshot,
-  imgMyPageScreenshot,
-};
+const imgObjLogo = (t: TranslationFn<"Images.Logo">) => ({
+  icoLogo: { ...icoLogo, alt: t("full") },
+  imgLogo: { ...imgLogo, alt: t("full") },
+});
+
+const pwaScreenshots = (t: TranslationFn<"Images.PWA-screenshots">) => ({
+  imgHomePageScreenshot: { ...imgHomePageScreenshot, alt: t("home") },
+  imgMyPageScreenshot: { ...imgMyPageScreenshot, alt: t("myPage") },
+});
+
+export { TITLE, TITLE_SHORT, TITLE_TEMPLATE, imgObjLogo, pwaScreenshots };
